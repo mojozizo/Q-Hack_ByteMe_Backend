c94a24a498093e0cdcdf7e19b9ceb7572d0a2045
Demyan Kurbatov <kurbat02@ads.uni-passau.de>
2025-04-23 15:55:28 +0200

    feat: added middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.controller import router
 
@@ -7,4 +8,12 @@ from api.controller import router
 load_dotenv()
 
 app = FastAPI(title="ByteMe - ACE Alternative")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev only; lock down in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router=router)
