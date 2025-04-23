01c8cb2e46a43277d155ab4d6aff7abe93e473ec
Demyan Kurbatov <kurbat02@ads.uni-passau.de>
2025-04-23 21:36:14 +0200

    chore: added token-methods for brightdata

--- etl/util/token_util.py
+++ etl/util/token_util.py
@@ -6,3 +6,15 @@ def get_newsapi_token() -> str:
     if not token:
         raise ValueError("NEWSAPI_API_TOKEN environment variable is not set.")
     return token
+
+def get_brightdata_token() -> str:
+    token = os.getenv("BRIGHTDATA_API_TOKEN")
+    if not token:
+        raise ValueError("BRIGHTDATA_API_TOKEN environment variable is not set.")
+    return token
+
+def get_brightdata_dataset_id() -> str:
+    dataset_id = os.getenv("BRIGHTDATA_DATASET_ID")
+    if not dataset_id:
+        raise ValueError("BRIGHTDATA_DATASET_ID environment variable is not set.")
+    return dataset_id
