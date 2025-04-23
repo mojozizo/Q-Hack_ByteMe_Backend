9dd2b9947280fe18c122647b140ae14a0dbe0ff7
Demyan Kurbatov <kurbat02@ads.uni-passau.de>
2025-04-23 15:50:02 +0200

    fix: fixed abstract_extracter.py

--- etl/extract/abstract_extracter.py
+++ etl/extract/abstract_extracter.py
@@ -2,16 +2,22 @@ import abc
 
 from typing import Dict, List, Optional
 
+from fastapi import Path
+
+
 class AbstractExtracter(abc.ABC):
     """
     Abstract base class for extracters.
     """
 
-    def __init__(self, config: Dict):
-        self.config = config
+    def __init__(self):
+        """
+        Initialize the extractor.
+        """
+        pass
 
     @abc.abstractmethod
-    def extract(self) -> List[Dict]:
+    def extract(self, file: Path, query: str) -> List[Dict]:
         """
         Extract data from a source.
 
